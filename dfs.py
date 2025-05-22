3310bc170014783eb9a50dd18031088e761ea046
Andrey Alperin <andreyalp@me.com>
2025-05-22 09:41:01 +0300

    initial

---
+++ dfs.py
@@ -0,0 +1,24 @@
+# Граф представлен в виде словаря смежности
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+# DFS с выводом порядка обхода
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    print(start)
+
+    for neighbor in graph[start]:
+        if neighbor not in visited:
+            dfs(graph, neighbor, visited)
+
+# Запуск DFS
+dfs(graph, 'A')
+
